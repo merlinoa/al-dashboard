299718792cf26650789a5e08e2d2e3b8e53c6033
Katie Kirkland <katie@Katies-MBP.hsd1.ga.comcast.net>
2015-04-12 20:37:01 -0400

    added summary and improved request tabs

--- server.R
+++ server.R
@@ -1,26 +1,78 @@
 ## server.R ##
-library(reshape2)
-library(dplyr)
-library(ggplot2)
-
-# connect to database
-al_db <- src_sqlite("al_db.sqlite3")
 
 function(input, output, session) {
   
-  # retreive vehicles from database
-  vehicles <- reactive({
-    holder <- tbl(al_db, sql("SELECT * FROM vehicles"))
-    as.data.frame(holder)
+  # vehicles summary ----------------------------------------------------------
+  vehicles_summary <- reactive({
+    # subset by active vehicles
+    if (identical(input$active_vehicles_summary, "active_vehicles_only")) {
+      summary <- vehicles[vehicles$delete_date >= input$vehicle_date_summary | 
+                       is.na(vehicles$delete_date), ]
+    } else {
+      summary <- vehicles
+    }
+    
+    summary <- group_by_(
+      summary,
+      input$group_by_vehicles
+    ) %>% summarise_(
+      vehicles = ~n(),
+      ACV = ~sum(acv, na.rm = TRUE)
+    )
+    
+    totals <- lapply(summary[, 2:3], sum, na.rm = TRUE)
+    
+    totals <- data.frame(
+      "Totals:", 
+      totals[1],
+      totals[2]
+    )
+    
+    names(totals) <- names(summary)
+    
+    rbind(
+      summary, 
+      totals
+    )
   })
   
-  # output vehicles table 
+  output$vehicles_summary_out <- renderDataTable({
+    vehicles_summary()
+  })
+  
+  # vechile summary table download
+  output$download_vehicles_summary <- downloadHandler(
+    filename = function() {
+      paste0(
+        "vehicle-summary-",
+        input$vehicle_date_summary,
+        ".csv")
+    },
+    content = function(file) {
+      write.csv(
+        vehicles_summary(),
+        file = file,
+        row.names = FALSE
+      )
+    }
+  )
+  
+  # All vehicles --------------------------------------------------------------- 
   vehicles_table <- reactive({
-    holder <- vehicles()[, setdiff(names(vehicles()), c("vehicle_id", "class"))]
-    if (input$active_vehicles) {
+    holder <- vehicles[, setdiff(names(vehicles), c("vehicle_id", "class"))]
+    # subset by active vehicles
+    if (identical(input$active_vehicles, "active_vehicles_only")) {
       holder <- holder[holder$delete_date >= input$vehicle_date | 
                       is.na(holder$delete_date), ]
     }
+    
+    # subset by member number
+    if ("All" %in% input$member_vehicles) {
+      # don't do anything
+    } else {
+      holder <- holder[holder$member_number %in% as.numeric(input$member_vehicles), ]
+    }
+    
     holder
   })
   
@@ -40,7 +92,39 @@ function(input, output, session) {
     }
   )
   
-  #---------------------Pricing-----------------------------------------
+  #------------------Request Form-------------------------------------
+  
+  request_sql <- reactive({
+    input$submit_request
+    isolate({
+      if (identical(input$request_type, add)) {
+        paste(
+          max(vehicles$vehicle_id) + 1,
+          input$vin_request,
+          input$year_request,
+          input$make_request,
+          input$model_request,
+          input$class_request,
+          input$acv_request,
+          input$add_date,
+          NULL,
+          Sys.time()
+        )
+      } else if (identical(input$request_type, delete)) {
+        # todo: delete request
+      } else {
+        # todo: change request
+      }
+    })
+  })
+  
+  
+  #dbSendQuery(
+  #  al_db$con, 
+  #  'INSERT INTO vehicles VALUES (9.9, 9.9, 9.9, 9.9, "new")'
+  #)
+  
+  #---------------------Rates-----------------------------------------
   
   # table to display base vehicle pricing rates for selected policy year
   base_rate <- reactive({
@@ -49,17 +133,4 @@ function(input, output, session) {
                    WHERE effective_end_date == NA"))
     as.data.frame(holder)
   })
-  
-  ### outputs to ui.r ---------------------------------------
-  
-  
-  # base rate table 
-  output$base <- renderTable({
-    base_rate()
-  },
-    include.rownames = FALSE,
-    align = "lllr",
-    digits = 2
-  )
-  
 }
--- ui.R
+++ ui.R
@@ -2,38 +2,97 @@
 library(shinydashboard)
 
 
-header <- dashboardHeader(title = "Auto Liability",
-            dropdownMenu(type = "notifications",
-              messageItem(
-                 from = "Enrty #6583",
-                 message = "Duplicate Vin found in database",
-                 icon = icon("question")
-              )
-            )
+header <- dashboardHeader(
+            title = "Auto Liability"
           )
 
 sidebar <- dashboardSidebar(
   sidebarMenu(
-    menuItem("Vehicles", tabName = "vehicles_tab", icon = icon("car")),
-    menuItem("Request Form", icon = icon("money"), tabName = "vehicle_request_form")
+    menuItem("Vehicles - Summary", tabName = "vehicles_summary_tab", icon = icon("car")),
+    menuItem("All Vehicles", tabName = "vehicles_tab", icon = icon("car")),
+    menuItem("Request Form", icon = icon("file-text"), tabName = "request_form_tab"),
+    menuItem("Rates", icon = icon("money"), tabName = "rates_tab")
   )
 )
 
 body <- dashboardBody(
   tabItems(
+    
+    # Vehicles Summary tab --------------------------------------------------------
+    tabItem(tabName = "vehicles_summary_tab",
+      fluidRow(
+        box(width = 4,
+          radioButtons(
+            inputId = "active_vehicles_summary",
+            label = NULL,  
+            choices = list(
+                       "All Vehicles" = "all_vehicles",
+                       "Active Vehicles Only" = "active_vehicles_only"
+                      )
+            ),
+          conditionalPanel(
+            condition = "input.active_vehicles_summary == 'active_vehicles_only'",
+            dateInput(
+              inputId = "vehicle_date_summary",
+              label = "Active Vehicles as of:",
+              value = Sys.Date()
+            )
+          )
+        ),
+        box(
+          width = 4,
+          radioButtons(
+            inputId = "group_by_vehicles",
+            label = "Summarize By:",
+            choices = list(
+                        "Member" = "member_number",
+                        "Vehicle Class" = "class"
+                      )
+          )
+        ),
+        box(
+          width = 4,
+          downloadButton(
+            outputId = "download_vehicles_summary", 
+            label = "Download Vehicles Summary"
+          )
+        ),
+        box(
+          width = 12,  
+          dataTableOutput("vehicles_summary_out")
+        )
+      )
+    ),
+  
+    
+  # Individual Vehicles tab -----------------------------------------------     
     tabItem(tabName = "vehicles_tab", 
       fluidRow(
         box(width = 4,
-          checkboxInput("active_vehicles", label = "Active Vehicles Only", value = TRUE)
-        ),
-        box(width = 4,
+          radioButtons(
+            inputId = "active_vehicles", 
+            label = NULL,  
+            choices = list(
+                        "All Vehicles" = "all_vehicles",
+                        "Active Vehicles Only" = "active_vehicles_only"
+                      )
+          ),
           conditionalPanel(
-            condition = "input.active_vehicles == true",
+            condition = "input.active_vehicles == 'active_vehicles_only'",
             dateInput(
               inputId = "vehicle_date",
               label = "Active Vehicles as of:",
               value = Sys.Date()
             )
+          )
+        ),
+        box(width = 4,
+          selectizeInput(
+            inputId = "member_vehicles",
+            label = "Member",
+            choices = c(unique(vehicles$member_number), "All"),
+            multiple = TRUE,
+            selected = "All"
           )
         ),
         box(width = 4,
@@ -44,30 +103,56 @@ body <- dashboardBody(
         )
       )
     ),
-    tabItem(tabName = "vehicle_request_form",
-            fluidRow(
-              column(width = 6,
-                radioButtons(inputId = "request_type", 
-                  label = "Request Type",
-                  choices = list("Add" = "add",
-                                 "Delete" = "delete",
-                                 "Change" = "change"
-                                 )
-                )
-              ),
-              
-              column(width = 6,
-                box(width = 12,
-                  h2("Current Pricing"),
-                  h3("Base Rates"),
-                  tableOutput("base_yr")
-                )
-              )
-            )
-    )
-    
   
-    
+  # Request for tab --------------------------------------------------------------
+    tabItem(
+      tabName = "request_form_tab",
+      fluidRow(
+        box(
+          width = 6,
+          h2("Vehicle Request Form"),
+          radioButtons(
+            inputId = "request_type", 
+            label = "Request Type",
+            choices = list(
+                        "Add" = "add",
+                        "Delete" = "delete",
+                        "Change" = "change"
+                      )
+          ),
+          textInput(
+            inputId = "vin_request",
+            label = "Vin #"
+          ),
+          textInput(
+            inputId = "year_request",
+            label = "Year"
+          ),
+          textInput(
+            inputId = "make_request",
+            label = "Make"
+          ),
+          textInput(
+            inputId = "model_request",
+            label = "Model"
+          ),
+          checkboxInput(
+            inputId = "class_request",
+            label = "Class"
+          ),
+          dateInput(
+            inputId = "date_request",
+            label = "Effective Date",
+            value = Sys.Date()
+          ),
+          actionButton("submit_request", "Submit Request")
+        )
+      )
+    ),
+  # ---- Rates tab ---------------------------------------------------------
+    tabItem(
+      tabName = "rates_tab"
+    )
   )
 )
 
