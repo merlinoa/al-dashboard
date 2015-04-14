507b868c1bf7c5acbb2676174b4cf2e688cf434c
merlinoa88@gmail.com <merlinoa88@gmail.com>
2015-04-13 22:26:08 -0400

    switch from sqlite to postgreSQL

--- global.R
+++ global.R
@@ -1,8 +1,15 @@
 library(dplyr)
 
 # connect to database
-al_db <- src_sqlite("al_db.sqlite3")
+al_db <- src_postgres(
+           dbname = "al_db",
+           host = "ractuary-db.carngedsdgcf.us-west-2.rds.amazonaws.com",
+           port = 5432,
+           user = "merlinoa",
+           password = "",
+           options = "-c search_path=al_db"
+         )
 
 # query all vehicles from data base
-vehicles <- tbl(al_db, sql("SELECT * FROM vehicles"))
+vehicles <- tbl(al_db, "vehicles")
 vehicles <- as.data.frame(vehicles)
